df69bd6b8f723620b747f2a7caccdf16fa25f4c6
cpohtato <clementpoh001@gmail.com>
2024-05-04 00:32:41 +1000

    do_mpc control loop

--- simulator/tvcLander.py
+++ simulator/tvcLander.py
@@ -10,7 +10,7 @@ class TVCLanderState():
         self.dz = -46.32 * dzMod
         self.beta = (-75.5 + betaMod) * math.pi / 180
         self.dbeta = dbetaMod * math.pi / 180
-        self.phi = (6.0 + phiMod) * math.pi / 180
+        self.phi = (0.0 + phiMod) * math.pi / 180
         self.dphi = dPhiMod * math.pi / 180
 
     def getVector(self):
@@ -156,12 +156,79 @@ class TVCLander():
             (self.currTau - self.b*dphi)/self.J_y
         ]
         return dxdt
+    
+    def trajStateTransition(self, x, t):
+        r_x, r_z, dx, dz, beta = x
+        T = 120 - t
+
+        dxdt = [
+            dx,
+            dz,
+            12*(0.0-r_x)/(pow(T, 2)) - 6*dx/T,
+            12*(50.0-r_z)/(pow(T, 2)) - 6*dz/T,
+            0
+        ]
+
+        return dxdt
 
     def simulate(self):
 
         global g
 
+        T_STEP = 0.1
+        TOT_TIME = 180
+        TIMESTEPS = round(TOT_TIME/0.1)
+        n_horizon = 30
+
+        trajectory = []
+        ic = self.state.getVector()
+        angle = math.atan2(ic[3], ic[4])
+        angle -= math.pi
+        trajIC = [ic[1], ic[2], ic[3], ic[4], angle]
+        trajectory.append(trajIC)
+
+        highGateSteps = math.floor(119.9/T_STEP)
+        for i in range(1, highGateSteps):
+            sol = odeint(self.trajStateTransition, trajectory[i-1], [(i-1)*0.1, i*0.1])
+            angle = math.atan2(sol[-1, 2], sol[-1, 3])
+            angle -= math.pi
+            vector = [sol[-1, 0], sol[-1, 1], sol[-1, 2], sol[-1, 3], angle]
+            trajectory.append(vector)
+
+        traj_z = 50.0
+        traj_dz = -1.83
+        vector = [0.0, traj_z, 0.0, traj_dz, 0.0]      #   Singularity at T = 120
+        trajectory.append(vector)
+
+        while (traj_z > 1.0):
+            traj_z += traj_dz * T_STEP
+            vector = [0.0, traj_z, 0.0, traj_dz, 0.0]
+            trajectory.append(vector)
+
+        while (len(trajectory) < TIMESTEPS + n_horizon):
+            vector = [0.0, traj_z, 0.0, 0.0, 0.0]
+            trajectory.append(vector)
+
+        arrR_x = []
+        arrR_z = []
+        arrR_dx = []
+        arrR_dz = []
+        arrR_beta = []
+
+        for vec in trajectory:
+            arrR_x.append(vec[0])
+            arrR_z.append(vec[1])
+            arrR_dx.append(vec[2])
+            arrR_dz.append(vec[3])
+            arrR_beta.append(vec[4])
+
         model = do_mpc.model.Model('continuous')
+
+        r_x = model.set_variable(var_type='_tvp', var_name='r_x')
+        r_z = model.set_variable(var_type='_tvp', var_name='r_z')
+        r_dx = model.set_variable(var_type='_tvp', var_name='r_dx')
+        r_dz = model.set_variable(var_type='_tvp', var_name='r_dz')
+        r_beta = model.set_variable(var_type='_tvp', var_name='r_beta')
 
         m = model.set_variable(var_type='_x', var_name='m', shape=(1,1))
         x = model.set_variable(var_type='_x', var_name='x', shape=(1,1))
@@ -189,20 +256,33 @@ class TVCLander():
         model.setup()
 
         mpc = do_mpc.controller.MPC(model)
+        n_horizon = 30
         setup_mpc = {
-            'n_horizon': 10,
-            't_step': 0.1,
+            'n_horizon': n_horizon,
+            't_step': T_STEP,
             'n_robust': 0,
             'store_full_solution': True,
         }
         mpc.set_param(**setup_mpc)
 
-        mterm = x**2 + z**2 + dx**2 + dz**2 + beta**2 + dbeta**2
-        lterm = x**2 + z**2 + dx**2 + dz**2 + beta**2 + dbeta**2
+        tvp_template = mpc.get_tvp_template()
+        def tvp_fun(t):
+            idx = math.floor(t/T_STEP)
+            for k in range(n_horizon+1):
+                tvp_template['_tvp', k, 'r_x'] = arrR_x[idx+k]
+                tvp_template['_tvp', k, 'r_z'] = arrR_z[idx+k]
+                tvp_template['_tvp', k, 'r_dx'] = arrR_dx[idx+k]
+                tvp_template['_tvp', k, 'r_dx'] = arrR_dz[idx+k]
+                tvp_template['_tvp', k, 'r_beta'] = arrR_beta[idx+k]
+            return tvp_template
+        
+
+        mterm = (r_x - x)**2 + (r_z - z)**2 + (r_dx - dx)**2 + (r_dz - dz)**2
+        lterm = mterm
         mpc.set_objective(mterm=mterm, lterm=lterm)
 
         mpc.set_rterm(
-            F = 1e-4,
+            F = 1e-5,
             tau = 1e-4
         )
 
@@ -211,16 +291,40 @@ class TVCLander():
         mpc.bounds['lower', '_u', 'tau'] = -self.tau_max
         mpc.bounds['upper', '_u', 'tau'] = self.tau_max
 
-        mpc.bounds['lower', '_x', 'x'] = self.m_min
-        mpc.bounds['lower', '_x', 'x'] = 0.0
+        mpc.bounds['lower', '_x', 'm'] = self.m_min
         mpc.bounds['lower', '_x', 'z'] = 0.0
         mpc.bounds['lower', '_x', 'phi'] = -self.phi_max
-        mpc.bounds['lower', '_x', 'phi'] = self.phi_max
+        mpc.bounds['upper', '_x', 'phi'] = self.phi_max
 
+        mpc.scaling['_x', 'm'] = 1000
+        mpc.scaling['_x', 'x'] = 1000
+        mpc.scaling['_x', 'z'] = 1000
+        mpc.scaling['_x', 'dx'] = 100
+        mpc.scaling['_x', 'dz'] = 100
+        mpc.scaling['_x', 'beta'] = 10
+        mpc.scaling['_x', 'dbeta'] = 10
+        mpc.scaling['_x', 'phi'] = 0.1
+        mpc.scaling['_x', 'dphi'] = 0.1
+        mpc.scaling['_u', 'F'] = 1000
+        mpc.scaling['_u', 'tau'] = 1000
+
+        mpc.set_tvp_fun(tvp_fun)
         mpc.setup()
 
         simulator = do_mpc.simulator.Simulator(model)
         simulator.set_param(t_step = 0.1)
+
+        sim_tvp_template = simulator.get_tvp_template()
+        def sim_tvp_fun(t):
+            idx = math.floor(t/T_STEP)
+            sim_tvp_template['r_x'] = arrR_x[idx]
+            sim_tvp_template['r_z'] = arrR_z[idx]
+            sim_tvp_template['r_dx'] = arrR_dx[idx]
+            sim_tvp_template['r_dz'] = arrR_dz[idx]
+            sim_tvp_template['r_beta'] = arrR_beta[idx]
+            return sim_tvp_template
+        simulator.set_tvp_fun(sim_tvp_fun)
+
         simulator.setup()
 
         ic = self.state.getVector()
@@ -229,35 +333,79 @@ class TVCLander():
         mpc.x0 = x0
         mpc.set_initial_guess()
 
-        mpl.rcParams['font.size'] = 18
-        mpl.rcParams['lines.linewidth'] = 3
+        mpl.rcParams['font.size'] = 8
+        mpl.rcParams['lines.linewidth'] = 2
         mpl.rcParams['axes.grid'] = True
         
         mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
         sim_graphics = do_mpc.graphics.Graphics(simulator.data)
 
-        fig, ax = plt.subplots(2, sharex=True, figsize=(16,9))
+        fig, ax = plt.subplots(3, 3, sharex=True, figsize=(10,8))
         fig.align_ylabels()
 
-        for g in [sim_graphics, mpc_graphics]:
-            g.add_line(var_type='_x', var_name='x', axis=ax[0])
-            g.add_line(var_type='_x', var_name='z', axis=ax[0])
+        for g in [sim_graphics]:
+            g.add_line(var_type='_x', var_name='x', axis=ax[0,0])
+            g.add_line(var_type='_x', var_name='z', axis=ax[0,0])
 
-            g.add_line(var_type='_u', var_name='F', axis=ax[1])
-            g.add_line(var_type='_u', var_name='tau', axis=ax[1])
+            g.add_line(var_type='_x', var_name='dx', axis=ax[1,0])
+            g.add_line(var_type='_x', var_name='dz', axis=ax[1,0])
 
-        ax[0].set_ylabel('Distance [m]')
-        ax[1].set_ylabel('Input')
-        ax[1].set_xlabel('Time [s]')
+            g.add_line(var_type='_u', var_name='F', axis=ax[2,0])
+
+            g.add_line(var_type='_x', var_name='beta', axis=ax[0,1])
+
+            g.add_line(var_type='_x', var_name='dbeta', axis=ax[1,1])
+
+            g.add_line(var_type='_x', var_name='m', axis=ax[2,1])
+
+            g.add_line(var_type='_x', var_name='phi', axis=ax[0,2])
+
+            g.add_line(var_type='_x', var_name='dphi', axis=ax[1,2])
+
+            g.add_line(var_type='_u', var_name='tau', axis=ax[2,2])
+
+        ax[0,0].set_ylabel('Distance [m]')
+        lines = sim_graphics.result_lines['_x', 'x'] + sim_graphics.result_lines['_x', 'z']
+        ax[0,0].legend(lines, ['x', 'z'])
+
+        ax[1,0].set_ylabel('Velocity [m/s]')
+        lines = sim_graphics.result_lines['_x', 'dx'] + sim_graphics.result_lines['_x', 'dz']
+        ax[1,0].legend(lines, ['dx', 'dz'])
+
+        ax[2,0].set_ylabel('Thrust [N]')
+        ax[2,0].legend(sim_graphics._result_lines['_u', 'F'], ['F'])
+        ax[2,0].set_xlabel('Time [s]')
+
+        ax[0,1].set_ylabel('Angle [rad]')
+        ax[0,1].legend(sim_graphics._result_lines['_x', 'beta'], ['beta'])
+
+        ax[1,1].set_ylabel('Velocity [rad/s]')
+        ax[1,1].legend(sim_graphics._result_lines['_x', 'dbeta'], ['dbeta'])
+
+        ax[2,1].set_ylabel('Mass [kg]')
+        ax[2,1].legend(sim_graphics._result_lines['_x', 'm'], ['m'])
+        ax[2,1].set_xlabel('Time [s]')
+
+        ax[0,2].set_ylabel('Gimbal Angle [rad]')
+        ax[0,2].legend(sim_graphics._result_lines['_x', 'phi'], ['phi'])
+
+        ax[1,2].set_ylabel('Gimbal Velocity [rad/s]')
+        ax[1,2].legend(sim_graphics._result_lines['_x', 'dphi'], ['dphi'])
+
+        ax[2,2].set_ylabel('Torque [Nm]')
+        ax[2,2].legend(sim_graphics._result_lines['_u', 'tau'], ['tau'])
+        ax[2,2].set_xlabel('Time [s]')
 
         simulator.reset_history()
         simulator.x0 = x0
+        x = x0
         mpc.reset_history()
-        for i in range(round(SIM_LENGTH/0.1)):
-            u0 = mpc.make_step(x0)
-            simulator.make_step(u0)
 
-        mpc_graphics.plot_predictions(t_ind=0)
+        for i in range(TIMESTEPS):
+            u = mpc.make_step(x)
+            x = simulator.make_step(u)
+
+        # mpc_graphics.plot_predictions(t_ind=0)
         sim_graphics.plot_results()
         sim_graphics.reset_axes()
 
